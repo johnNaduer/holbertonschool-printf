cc4d8f0aba2dce12cd4fb6929cd46f1aa4230943
John Naduer <naduer789@gmail.com>
2022-11-12 15:31:11 -0800

    Write a function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -5,13 +5,17 @@ int _printf(const char *format, ...)
 {
 va_list arg;
 	functions funciones[]={{'c',funcion_c},{'s',funcion_s},{'d',funcion_i},{'i',funcion_i},{'\0',NULL}};
-	int i, j, leng1, leng2;
+	int i, j, leng1, leng2,l;
 	void (*p)(char);
 
 	leng1 = 0;
 	leng2 = 0;
-	
+	l=0;
 	p = &imprimir;
+	if(format == NULL)
+	{
+		return (-1);
+	}
 	va_start(arg, format);
 
 	for(i=0 ;format[i]!= '\0';i++)
@@ -19,6 +23,13 @@ va_list arg;
 
     	if(format[i]=='%')
     	{
+	
+		if(format[i+1]=='%')
+		{
+			_putchar('%');
+			l++;
+		}
+
         i++;
         if(format[i]=='%')
         {
@@ -39,5 +50,5 @@ va_list arg;
     }
 }
 va_end(arg);
-return (leng1 + leng2);
+return (leng1 + leng2+ l);
 }
